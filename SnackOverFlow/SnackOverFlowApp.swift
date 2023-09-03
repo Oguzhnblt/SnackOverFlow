74e7cdae0401ffa2c5688d1b53c27caf8f48e7ac
Oguzhnblt <oguzhanbolat0@gmail.com>
2023-09-03 12:45:40 +0300

    Initial Commit

---
+++ SnackOverFlow/SnackOverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackOverFlowApp.swift
+//  SnackOverFlow
+//
+//  Created by Oğuz on 3.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnackOverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
